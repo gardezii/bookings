eba8b087e3fd5de903fb2cef84571a121cefa44b
gardezii <ali.gardezi@tintash.com>
2020-06-23 05:21:21 +0500

    serializer fields updated

--- bookingapi/serializers.py
+++ bookingapi/serializers.py
@@ -2,21 +2,7 @@ from rest_framework import serializers
 
 from .models import Booking
 
-class BookingSerializer(serializers.HyperlinkedModelSerializer):
+class BookingSerializer(serializers.ModelSerializer):
 		class Meta:
 			model = Booking
-			fields = (
-				'id', 
-				'name',
-				'location',
-				'slotKey',
-				'bookingKey',
-				'date',
-				'duration',
-				'pitch',
-				'rate',
-				'start',
-				'end',
-				'status',
-				'sumittedDate'
-			)
+			fields = '__all__'
